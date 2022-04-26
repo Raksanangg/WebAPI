e897f0a966597961d639500167b4f7e1bbed7498
Raksanangg <raksanan.gg@gmail.com>
2022-04-26 16:02:45 +0700

    End of section 8

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;        
+        public string PhotoUrl {get; set;} = string.Empty;
+        public int Age { get; set; }
+        public string KnownAs { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Gender { get; set; } = string.Empty;
+        public string Introduction { get; set; } = string.Empty;
+        public string LookingFor { get; set; } = string.Empty;
+        public string Interests { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public ICollection<PhotoDto> Photos { get; set; } = default!; 
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -13,6 +14,33 @@ namespace API.Entities
        public byte[] PasswordHash {get; set;} = default!;
 
        public byte[] PasswordSalt {get; set;} = default!;
+
+       public DateTime DateOfBirth {get; set;}
+
+       public string KnownAs {get; set;} = string.Empty;
+
+       public DateTime Created {get; set;} = DateTime.Now;
+
+       public DateTime LastActive {get; set;} = DateTime.Now;
+
+       public string Gender {get; set;} = string.Empty;
+
+       public string Introduction {get; set;} = string.Empty;
+
+       public string LookingFor {get; set;} = string.Empty;
+
+       public string Interests {get; set;} = string.Empty;
+
+       public string City {get; set;} = string.Empty;
+
+       public string Country {get; set;} = string.Empty;
+
+       public ICollection<Photo> Photos {get; set;} = default!;      
+
+    //    public int GetAge()
+    //     {
+    //         return DateOfBirth.CalculateAge();
+    //     }
       
     }
 }
